98477753ace9d39465e6560f47089249d96b369c
Keith Kaplan <keithkaplan0@gmail.com>
2024-09-21 13:55:36 -0700

    Display sample layout

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::io;
 use crossterm::event::{self, KeyCode, KeyEventKind};
 use ratatui::{
-    buffer::Buffer,
-    layout::Rect,
+    layout::{Constraint, Layout},
+    style::Stylize,
+    text::Line,
     widgets::{
-        block::{Block, BorderType},
+        block::Block,
+        Borders,
         Paragraph,
-        Widget,
     },
     DefaultTerminal,
+    Frame,
 };
 
 struct Model {
@@ -28,18 +30,38 @@ impl Model {
     }
 }
 
-fn view(_model: &Model) -> Paragraph {
-    let block = Block::bordered()
-        .border_type(BorderType::Thick);
-    Paragraph::new("Hello, world!")
+fn view(_model: &Model, frame: &mut Frame) {
+    let [top_item_area, tree_area, status_area, command_key_area] =
+        Layout::vertical([
+            Constraint::Length(3),
+            Constraint::Min(0),
+            Constraint::Length(1),
+            Constraint::Length(1),
+        ])
+        .areas(frame.area());
+    let top_item = Paragraph::new("Top Item".bold())
         .centered()
-        .block(block)
-}
-
-impl Widget for &Model {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        view(self).render(area,buf);
-    }
+        .on_black()
+        .block(Block::new().borders(Borders::ALL));
+    let tree = Paragraph::new("Hello, world!")
+        .centered()
+        .on_black();
+    let status = Line::from(" Top item selected.")
+        .on_dark_gray();
+    let command_key = Line::from(vec![
+            " I ".black().on_white().bold(),
+            " Insert    ".italic(),
+            " D ".black().on_white().bold(),
+            " Delete    ".italic(),
+            " Q ".black().on_white().bold(),
+            " Quit".italic(),
+        ])
+        .centered()
+        .on_black();
+    frame.render_widget(top_item, top_item_area);
+    frame.render_widget(tree, tree_area);
+    frame.render_widget(status, status_area);
+    frame.render_widget(command_key, command_key_area);
 }
 
 fn handle_event() -> io::Result<Message> {
@@ -64,7 +86,7 @@ fn update(model: &mut Model, msg: Message) {
 fn main_loop(mut terminal: DefaultTerminal) -> io::Result<()> {
     let mut model = Model::new();
     while !model.quit {
-        terminal.draw(|frame| frame.render_widget(&model, frame.area()))?;
+        terminal.draw(|frame| view(&model, frame))?;
         let msg = handle_event()?;
         update(&mut model, msg);
     }
