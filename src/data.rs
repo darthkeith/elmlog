51a0fe6b740fc0f5dff5796e9db41ca117eeadd2
Keith Kaplan <keithkaplan0@gmail.com>
2024-09-22 01:03:53 -0700

    Add `prepend` function

--- src/data.rs
+++ src/data.rs
@@ -3,24 +3,29 @@ type Heap = Option<Box<Node>>;
 /// A node in a left-child right-sibling binary tree
 struct Node {
     label: String,
-    size: usize,
     child: Heap,
     sibling: Heap,
+    size: usize,
 }
 
-fn size(heap: &Heap) -> usize {
+fn heap_size(heap: &Heap) -> usize {
     match heap {
         Some(ref node) => node.size,
         None => 0,
     }
 }
 
-fn new(label: String, child: Heap, sibling: Heap) -> Node {
-    Node {
-        label: label,
-        size: 1 + size(&child) + size(&sibling),
-        child: child,
-        sibling: sibling,
-    }
+fn new(label: String, child: Heap, sibling: Heap) -> Box<Node> {
+    let size = 1 + heap_size(&child) + heap_size(&sibling);
+    let node = Node {
+        label,
+        child,
+        sibling,
+        size,
+    };
+    Box::new(node)
 }
 
+fn prepend(root: Heap, label: String) -> Heap {
+    Some(new(label, None, root))
+}
