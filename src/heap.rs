ea75b1998a245160ff0258b62242a7f6b2fb03e6
Keith Kaplan <keithkaplan0@gmail.com>
2024-09-26 08:51:55 -0700

    Add documentation comments to heap module

--- src/heap.rs
+++ src/heap.rs
@@ -1,8 +1,17 @@
+/// An ordered forest of multi-way trees containing strings.
+///
+/// Each node can have any number of children and siblings (the roots are
+/// siblings of each other). The data structure is implemented as a left-child
+/// right-sibling binary tree.
 pub struct Heap {
     root: Option<Box<Node>>,
 }
 
-/// A node in a left-child right-sibling binary tree
+/// A node in a left-child right-sibling binary tree containing a string.
+///
+/// In the binary tree, `size` is the number of nodes in the node's subtree.
+/// In the represented heap, `size` is the number of nodes in the node's
+/// subtree plus the number of nodes in all subsequent sibling subtrees.
 struct Node {
     label: String,
     child: Heap,
@@ -10,17 +19,21 @@ struct Node {
     size: usize,
 }
 
-/// Represents the number of roots in a heap (none, one, or multiple)
+/// Represents the number of roots in a heap (zero, one, or multiple).
+///
+/// When there is one root, a reference to its label is stored.
 pub enum HeapStatus<'a> {
     Empty,
     SingleRoot(&'a str),
     MultiRoot,
 }
 
+/// Contstruct an empty heap.
 pub fn empty() -> Heap {
     Heap { root: None }
 }
 
+/// Return the number of nodes in the `heap`.
 pub fn heap_size(heap: &Heap) -> usize {
     match heap.root {
         Some(ref node) => node.size,
@@ -28,6 +41,7 @@ pub fn heap_size(heap: &Heap) -> usize {
     }
 }
 
+/// Construct a new node given its `label`, left `child`, and right `sibling`.
 fn new_node(label: String, child: Heap, sibling: Heap) -> Box<Node> {
     let size = 1 + heap_size(&child) + heap_size(&sibling);
     let node = Node {
@@ -39,11 +53,13 @@ fn new_node(label: String, child: Heap, sibling: Heap) -> Box<Node> {
     Box::new(node)
 }
 
+/// Insert a node with given `label` before the first tree in the `heap`.
 pub fn prepend(heap: Heap, label: String) -> Heap {
     let root = Some(new_node(label, empty(), heap));
     Heap { root }
 }
 
+/// Iterator type for iterating over a heap's labels in pre-order.
 pub struct PreOrderIter<'a> {
     stack: Vec<&'a Node>,
 }
@@ -65,6 +81,7 @@ impl<'a> Iterator for PreOrderIter<'a> {
     }
 }
 
+/// Return an iterator over the `heap`'s labels in pre-order.
 pub fn iter(heap: &Heap) -> PreOrderIter {
     let mut stack = Vec::new();
     if let Some(root) = &heap.root {
@@ -73,6 +90,7 @@ pub fn iter(heap: &Heap) -> PreOrderIter {
     PreOrderIter { stack }
 }
 
+/// Return the status of the `heap` (if there is one root, include its label).
 pub fn status(heap: &Heap) -> HeapStatus {
     match &heap.root {
         None => HeapStatus::Empty,
