3470196aa14dd7f793282d060e860cd95d7ab409
Keith Kaplan <keithkaplan0@gmail.com>
2024-09-27 23:22:35 -0700

    Refactor view into smaller functions

--- src/ui.rs
+++ src/ui.rs
@@ -13,33 +13,34 @@ use ratatui::{
 use crate::{Mode, Model};
 use crate::heap::HeapStatus;
 
-pub fn view(model: &Model, frame: &mut Frame) {
-    let [top_item_area, tree_area, status_area, command_key_area] =
-        Layout::vertical([
-            Constraint::Length(3),
-            Constraint::Min(0),
-            Constraint::Length(1),
-            Constraint::Length(1),
-        ])
-        .areas(frame.area());
+/// Return the top item widget using the current `model`.
+fn top_item(model: &Model) -> Paragraph {
     let top_item_str = match model.heap.status() {
         HeapStatus::SingleRoot(label) => format!(" {label} "),
-        _ => "".to_string(),
+        _ => String::new(),
     };
-    let top_item = Paragraph::new(top_item_str.black().on_white().bold())
+    Paragraph::new(top_item_str.black().on_white().bold())
         .block(Block::new().borders(Borders::ALL))
         .centered()
-        .on_black();
+        .on_black()
+}
+
+/// Return the forest widget using the current `model`.
+fn forest(model: &Model) -> Text {
     let idx_len = match model.heap.size() {
         0 => 0,
         n => (n - 1).to_string().len(),
     };
-    let tree_lines = model.heap.iter()
+    let forest_lines = model.heap.iter()
         .enumerate()
         .map(|(i, label)| format!(" {i:>width$}   {label}", width = idx_len));
-    let tree = Text::from_iter(tree_lines)
+    Text::from_iter(forest_lines)
         .left_aligned()
-        .on_black();
+        .on_black()
+}
+
+/// Return the status widget using the current `model`.
+fn status(model: &Model) -> Line {
     let status_msg = match model.mode {
         Mode::Normal => match model.heap.status() {
             HeapStatus::Empty => " Empty.".to_string(),
@@ -48,9 +49,13 @@ pub fn view(model: &Model, frame: &mut Frame) {
         }
         Mode::Input(ref label) => format!(" > {label}"),
     };
-    let status = Line::from(status_msg)
+    Line::from(status_msg)
         .left_aligned()
-        .on_dark_gray();
+        .on_dark_gray()
+}
+
+/// Return the command key widget using the current `model`.
+fn command_key(model: &Model) -> Line {
     let command_keys = match model.mode {
         Mode::Normal => vec![
             " I ".black().on_white().bold(),
@@ -67,12 +72,24 @@ pub fn view(model: &Model, frame: &mut Frame) {
             " Cancel ".italic(),
         ],
     };
-    let command_key = Line::from(command_keys)
+    Line::from(command_keys)
         .centered()
-        .on_black();
-    frame.render_widget(top_item, top_item_area);
-    frame.render_widget(tree, tree_area);
-    frame.render_widget(status, status_area);
-    frame.render_widget(command_key, command_key_area);
+        .on_black()
+}
+
+/// Render the UI on the `frame` using the current `model`.
+pub fn view(model: &Model, frame: &mut Frame) {
+    let [top_item_area, forest_area, status_area, command_key_area] =
+        Layout::vertical([
+            Constraint::Length(3),
+            Constraint::Min(0),
+            Constraint::Length(1),
+            Constraint::Length(1),
+        ])
+        .areas(frame.area());
+    frame.render_widget(top_item(model), top_item_area);
+    frame.render_widget(forest(model), forest_area);
+    frame.render_widget(status(model), status_area);
+    frame.render_widget(command_key(model), command_key_area);
 }
 
