e4b746d679839543e08b1386fdad89f60b7da473
Keith Kaplan <keithkaplan0@gmail.com>
2024-10-07 19:28:22 -0700

    Refactor command bar constructor

--- src/view.rs
+++ src/view.rs
@@ -84,8 +84,8 @@ fn forest(model: &Model) -> Text {
         .on_black()
 }
 
-// Return the status widget using the current `model`.
-fn status(model: &Model) -> Line {
+// Return the status bar widget using the current `model`.
+fn status_bar(model: &Model) -> Line {
     let status_msg = match model.mode {
         Mode::Normal => match model.heap.status() {
             HeapStatus::Empty => " Empty.".to_string(),
@@ -101,40 +101,43 @@ fn status(model: &Model) -> Line {
         .on_dark_gray()
 }
 
-// Return the command key widget using the current `model`.
-fn command_key(model: &Model) -> Line {
-    let command_keys = match model.mode {
-        Mode::Normal => vec![
-            " I ".black().on_white().bold(),
-            " Insert    ".italic(),
-            " D ".black().on_white().bold(),
-            " Delete    ".italic(),
-            " M ".black().on_white().bold(),
-            " Merge    ".italic(),
-            " Q ".black().on_white().bold(),
-            " Quit".italic(),
-        ],
-        Mode::Input(_) | Mode::Delete(_) => vec![
-            " Enter ".black().on_white().bold(),
-            " Submit    ".italic(),
-            " Esc ".black().on_white().bold(),
-            " Cancel ".italic(),
-        ],
-        Mode::Merge => vec![
-            " Up ".black().on_white().bold(),
-            " First    ".italic(),
-            " Down ".black().on_white().bold(),
-            " Second".italic(),
-        ],
-    };
-    Line::from(command_keys)
+// Convert key-command pairs into a command bar.
+fn make_command_bar<'a>(pairs: Vec<(&'a str, &'a str)>) -> Line<'a> {
+    let mut text_spans = Vec::new();
+    for (key, command) in pairs {
+        text_spans.push(format!(" {key} ").black().on_white().bold());
+        text_spans.push(format!(" {command}").italic());
+        text_spans.push("    ".into());
+    }
+    text_spans.pop();  // Remove extra spacer at end
+    Line::from(text_spans)
         .centered()
         .on_black()
 }
 
+// Return the command bar widget based on the current `model`.
+fn command_bar(model: &Model) -> Line {
+    make_command_bar(match model.mode {
+        Mode::Normal => vec![
+            ("I", "Insert"),
+            ("D", "Delete"),
+            ("M", "Merge"),
+            ("Q", "Quit"),
+        ],
+        Mode::Input(_) | Mode::Delete(_) => vec![
+            ("Enter", "Submit"),
+            ("Esc", "Cancel"),
+        ],
+        Mode::Merge => vec![
+            ("Up", "First"),
+            ("Down", "Second"),
+        ],
+    })
+}
+
 /// Render the UI on the `frame` using the current `model`.
 pub fn view(model: &Model, frame: &mut Frame) {
-    let [top_item_area, forest_area, status_area, command_key_area] =
+    let [top_item_area, forest_area, status_bar_area, command_bar_area] =
         Layout::vertical([
             Constraint::Length(3),
             Constraint::Min(0),
@@ -144,7 +147,7 @@ pub fn view(model: &Model, frame: &mut Frame) {
         .areas(frame.area());
     frame.render_widget(top_item(model), top_item_area);
     frame.render_widget(forest(model), forest_area);
-    frame.render_widget(status(model), status_area);
-    frame.render_widget(command_key(model), command_key_area);
+    frame.render_widget(status_bar(model), status_bar_area);
+    frame.render_widget(command_bar(model), command_bar_area);
 }
 
