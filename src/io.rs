af78cb10d16c2f56218e985be4da25244f14c748
Keith Kaplan <keithkaplan0@gmail.com>
2024-10-27 05:47:13 -0700

    Make data file read only

--- src/io.rs
+++ src/io.rs
@@ -1,6 +1,6 @@
 use std::{
-    fs::{File, OpenOptions},
-    io::{Read, Result, Seek, SeekFrom},
+    fs::{self, File, OpenOptions},
+    io::{Read, Result},
     path::Path,
 };
 
@@ -12,18 +12,27 @@ use crate::{
 
 const APP_DATA_FILE: &str = ".app_data";
 
-/// Return the initialized heap with its associated data file.
-pub fn init() -> Result<(Heap, File)> {
-    let file_path = Path::new(APP_DATA_FILE);
-    let file_exists = file_path.exists();
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(file_path)?;
+/// Return the Path to the application data file.
+pub fn data_file_path() -> &'static Path {
+    Path::new(APP_DATA_FILE)
+}
+
+fn lock(file: &File) {
     file.try_lock_exclusive()
         .expect("Application data file is currently locked");
-    if !file_exists {
+}
+
+/// Return the initialized heap with its associated data file.
+pub fn init(file_path: &Path) -> Result<(Heap, File)> {
+    let file_not_found = !file_path.exists();
+    if file_not_found {
+        File::create(file_path)?;
+    }
+    let mut file = OpenOptions::new()
+        .read(true)
+        .open(file_path)?;
+    lock(&file);
+    if file_not_found {
         return Ok((Heap::Empty, file));
     }
     let mut buffer = Vec::new();
@@ -33,12 +42,24 @@ pub fn init() -> Result<(Heap, File)> {
     Ok((heap, file))
 }
 
-/// Save the `heap` to the data `file`.
-pub fn save(heap: Heap, mut file: File) -> Result<()> {
-    file.set_len(0)?;
-    file.seek(SeekFrom::Start(0))?;
+fn set_writable(file_path: &Path, writable: bool) -> Result<()> {
+    let file = File::open(file_path)?;
+    let metadata = file.metadata()?;
+    let mut permissions = metadata.permissions();
+    permissions.set_readonly(!writable);
+    fs::set_permissions(file_path, permissions)
+}
+
+/// Save the `heap` to the file with given Path.
+pub fn save(heap: Heap, file_path: &Path) -> Result<()> {
+    set_writable(file_path, true)?;
+    let file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(file_path)?;
+    lock(&file);
     bincode::serialize_into(&file, &heap)
-        .expect("Failed to serialise data");
-    file.unlock()
+        .expect("Failed to serialize data");
+    set_writable(file_path, false)
 }
 
