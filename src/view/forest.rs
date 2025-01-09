1327a2d6dfc21c51f0ab5b31ac75ca501a3e1fa9
Keith Kaplan <keithkaplan0@gmail.com>
2025-01-09 15:44:22 -0800

    Remove root context colors

--- src/view/forest.rs
+++ src/view/forest.rs
@@ -1,5 +1,4 @@
 use ratatui::{
-    style::Style,
     text::{Line, Span, Text},
     widgets::Paragraph,
 };
@@ -7,7 +6,6 @@ use ratatui::{
 use crate::{
     heap::{
         Heap,
-        HeapStatus,
         NodePosition,
         NodeType,
         PreOrderIter,
@@ -25,18 +23,10 @@ enum IndentBlock {
     VertBar,
 }
 
-// Indicates what style to apply to a label.
-enum LabelType {
-    SingleRoot,
-    Root,
-    Child,
-}
-
 // Iterator type returning the strings used to display the forest.
 struct ForestIter<'a> {
     prefix: Vec<IndentBlock>,
     label_iter: PreOrderIter<'a>,
-    single_root: bool,
 }
 
 impl<'a> ForestIter<'a> {
@@ -44,28 +34,26 @@ impl<'a> ForestIter<'a> {
         ForestIter {
             prefix: Vec::new(),
             label_iter: heap.iter(),
-            single_root: matches!(heap.status(), HeapStatus::SingleRoot),
         }
     }
 }
 
 impl<'a> Iterator for ForestIter<'a> {
-    type Item = (String, &'a str, LabelType);
+    type Item = (String, &'a str);
 
     fn next(&mut self) -> Option<Self::Item> {
         let (label, pos) = self.label_iter.next()?;
         let NodePosition { node_type, is_last } = pos;
         let mut tree_row = String::new();
-        if let NodeType::Root = node_type {
-            self.prefix.clear();
-            let label_type = match self.single_root {
-                true => LabelType::SingleRoot,
-                false => LabelType::Root,
-            };
-            return Some((tree_row, label, label_type));
-        }
-        if let NodeType::Sibling = node_type {
-            while let Some(IndentBlock::Spacer) = self.prefix.pop() {}
+        match node_type {
+            NodeType::Root => {
+                self.prefix.clear();
+                return Some((tree_row, label));
+            }
+            NodeType::Sibling => {
+                while let Some(IndentBlock::Spacer) = self.prefix.pop() {}
+            }
+            NodeType::Child => (),
         }
         for block in &self.prefix {
             tree_row.push_str(match block {
@@ -80,35 +68,17 @@ impl<'a> Iterator for ForestIter<'a> {
             tree_row.push_str("├──");
             self.prefix.push(IndentBlock::VertBar);
         }
-        Some((tree_row, label, LabelType::Child))
-    }
-}
-
-// Return the style to apply to a label of given type with optional highlight.
-fn get_label_style(label_type: LabelType, highlight: bool) -> Style {
-    if highlight {
-        match label_type {
-            LabelType::SingleRoot => style::SINGLE_ROOT_HL,
-            LabelType::Root => style::ROOT_HL,
-            LabelType::Child => style::DEFAULT_HL,
-        }
-    } else {
-        match label_type {
-            LabelType::SingleRoot => style::SINGLE_ROOT,
-            LabelType::Root => style::ROOT,
-            LabelType::Child => style::DEFAULT,
-        }
+        Some((tree_row, label))
     }
 }
 
 /// Return the forest widget in normal mode.
 pub fn forest_normal(heap: &Heap) -> Paragraph {
     let lines = ForestIter::new(heap)
-        .map(|(tree_row, label, label_type)| {
-            let label_style = get_label_style(label_type, false);
+        .map(|(tree_row, label)| {
             Line::from(vec![
                 Span::styled(tree_row, style::TREE),
-                Span::styled(format!("{label} "), label_style),
+                Span::raw(label),
             ])
         });
     main_paragraph(Text::from_iter(lines))
@@ -119,19 +89,23 @@ pub fn forest_select(heap: &Heap, current_idx: usize) -> Paragraph {
     let index_len = util::max_index_length(heap.size());
     let lines = ForestIter::new(heap)
         .enumerate()
-        .map(|(i, (tree_row, label, label_type))| {
+        .map(|(i, (tree_row, label))| {
             let fmt_index = format!(" {i:>width$}   ", width = index_len);
             let highlight = i == current_idx;
-            let label_style = get_label_style(label_type, highlight);
-            let tree_style = match highlight {
-                true => style::TREE_HL,
-                false => style::TREE,
+            let spans = if highlight {
+                vec![
+                    Span::styled(fmt_index, style::DEFAULT_HL),
+                    Span::styled(tree_row, style::TREE_HL),
+                    Span::styled(format!("{label} "), style::DEFAULT_HL),
+                ]
+            } else {
+                vec![
+                    Span::raw(fmt_index),
+                    Span::styled(tree_row, style::TREE),
+                    Span::raw(label),
+                ]
             };
-            Line::from(vec![
-                Span::styled(fmt_index, label_style),
-                Span::styled(tree_row, tree_style),
-                Span::styled(format!("{label} "), label_style),
-            ])
+            Line::from(spans)
         });
     main_paragraph(Text::from_iter(lines))
 }
@@ -140,16 +114,15 @@ pub fn forest_select(heap: &Heap, current_idx: usize) -> Paragraph {
 pub fn forest_selected(heap: &Heap, current_idx: usize) -> Paragraph {
     let lines = ForestIter::new(heap)
         .enumerate()
-        .map(|(i, (tree_row, label, label_type))| {
+        .map(|(i, (tree_row, label))| {
             let highlight = i == current_idx;
-            let fmt_label = match highlight {
-                true => format!(" {label} "),
-                false => format!("{label} "),
+            let label_span = match highlight {
+                true => Span::styled(format!(" {label} "), style::DEFAULT_HL),
+                false => Span::raw(label),
             };
-            let label_style = get_label_style(label_type, highlight);
             Line::from(vec![
                 Span::styled(tree_row, style::TREE),
-                Span::styled(fmt_label, label_style),
+                label_span,
             ])
         });
     main_paragraph(Text::from_iter(lines))
--- src/view/style.rs
+++ src/view/style.rs
@@ -11,9 +11,6 @@ const FG: Color = Color::Rgb(250, 238, 202);
 const FG_DARK: Color = Color::Rgb(248, 232, 180);
 const GRAY: Color = Color::Rgb(109, 109, 115);
 const ORANGE: Color = Color::Rgb(255, 195, 74);
-const ORANGE_DARK: Color = Color::Rgb(82, 54, 0);
-const GREEN: Color = Color::Rgb(214, 217, 61);
-const GREEN_DARK: Color = Color::Rgb(88, 89, 0);
 
 pub const DEFAULT: Style = Style::new().fg(FG).bg(BG);
 pub const DEFAULT_HL: Style = Style::new().fg(BG_DARK).bg(FG);
@@ -22,14 +19,6 @@ pub const NUMBER: Style = Style::new().fg(ORANGE).bg(BG2)
     .add_modifier(Modifier::BOLD);
 pub const TREE: Style = Style::new().fg(GRAY).bg(BG);
 pub const TREE_HL: Style = Style::new().fg(GRAY).bg(FG);
-pub const SINGLE_ROOT: Style = Style::new().fg(GREEN).bg(BG)
-    .add_modifier(Modifier::BOLD);
-pub const SINGLE_ROOT_HL: Style = Style::new().fg(GREEN_DARK).bg(FG)
-    .add_modifier(Modifier::BOLD);
-pub const ROOT: Style = Style::new().fg(ORANGE).bg(BG)
-    .add_modifier(Modifier::BOLD);
-pub const ROOT_HL: Style = Style::new().fg(ORANGE_DARK).bg(FG)
-    .add_modifier(Modifier::BOLD);
 pub const CURSOR: Style = DEFAULT.add_modifier(Modifier::SLOW_BLINK);
 pub const CMD_KEY: Style = Style::new().fg(BG_DARK).bg(FG_DARK)
     .add_modifier(Modifier::BOLD);
