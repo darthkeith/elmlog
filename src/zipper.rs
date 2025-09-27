d5fb442b780bdf9748ff7cef5ee1b4b3c0e06fdb
Keith Kaplan <keithkaplan0@gmail.com>
2025-09-27 02:20:14 -0700

    Add move_backward method to FocusNode

--- src/zipper.rs
+++ src/zipper.rs
@@ -25,7 +25,7 @@ struct FocusNode {
 }
 
 impl FocusNode {
-    // Swap the focused subtree with its next sibling's (if present).
+    // Swap the focused node's subtree with its next sibling's (if present).
     pub fn move_forward(self) -> Self {
         match self.sibling {
             Some(sib_node) => {
@@ -34,12 +34,35 @@ impl FocusNode {
                     kind: PathNodeKind::Sibling{ child: sib_node.child },
                     label: sib_node.label,
                 };
-                FocusNode {
+                Self {
                     context: Some(Box::new(path_node)),
                     child: self.child,
                     sibling: sib_node.sibling,
                     label: self.label,
                 }
+            }
+            None => self,
+        }
+    }
+
+    // Swap the focused node's subtree with its previous sibling's (if present).
+    pub fn move_backward(self) -> Self {
+        match self.context {
+            Some(path_node) => match path_node.kind {
+                PathNodeKind::Sibling { child: prev_sib_child } => {
+                    let new_sibling = Node {
+                        child: prev_sib_child,
+                        sibling: self.sibling,
+                        label: path_node.label,
+                    };
+                    Self {
+                        context: path_node.context,
+                        child: self.child,
+                        sibling: Some(Box::new(new_sibling)),
+                        label: self.label,
+                    }
+                }
+                _ => Self { context: Some(path_node), ..self },
             }
             None => self,
         }
